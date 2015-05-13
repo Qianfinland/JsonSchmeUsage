3c945058f0625f1a66cf6c9b7042be1e5e1c3411
Qian Zhou <qian.zhou@npsopt.com>
2015-05-13 15:38:31 +0300

    uncomment the Car class

--- GenerateJsonSchema/Car.cs
+++ GenerateJsonSchema/Car.cs
@@ -1,15 +1,15 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace GenerateJsonSchema
-//{
-//    class Car
-//    {
-//        public string Model { get; set; }
-//        public int Year { get; set; }
-//        public string Color { get; set; }
-//    }
-//}
+namespace GenerateJsonSchema
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
